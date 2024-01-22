2c5c010cddd791c31b89bac4408cfbfb24d62043
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-22 22:24:34 +0800

    finished code

--- exercise 3.py
+++ exercise 3.py
@@ -1,7 +1,8 @@
 #Exercise 3: Print characters from a string that are present at an even index number
 #Write a program to accept a string from the user and display characters that are present at an even index number.
 
-pynative = ("pynative")
-print(pynative)
-for i in range(0, 8, 2):
-    print(pynative[i])
+word_input = input("Input Word ")
+print("your word is ", word_input)
+length_of_the_word = len(word_input)
+for i in range(0, length_of_the_word, 2):
+    print(word_input[i])
