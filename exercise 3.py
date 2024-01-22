d97c745d5ea8b3113fe87aabf3b9b48d52f3a606
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-22 22:05:22 +0800

    print pynative, not working for loop

--- exercise 3.py
+++ exercise 3.py
@@ -1,2 +1,7 @@
 #Exercise 3: Print characters from a string that are present at an even index number
 #Write a program to accept a string from the user and display characters that are present at an even index number.
+
+pynative = ("pynative")
+print(pynative)
+for i in range(0, 8, 2):
+    print(pynative,{i})
